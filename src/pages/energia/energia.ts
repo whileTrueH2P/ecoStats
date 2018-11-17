35c169bd36f2b6fb6f546c8fa87a96ca6c1421bd
whileTrue <whileTrue.ecoStats@gmail.com>
2018-11-17 16:08:51 +0100

    diseño de las páginas con bd

--- src/pages/energia/energia.ts
+++ src/pages/energia/energia.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireList } from 'angularfire2/database';
 
 @IonicPage()
 @Component({
@@ -8,12 +9,15 @@ import { AngularFireDatabase } from 'angularfire2/database';
   templateUrl: 'energia.html',
 })
 export class EnergiaPage {
-
+  private lista: AngularFireList<any>;
   constructor(private db: AngularFireDatabase, public navCtrl: NavController, public navParams: NavParams) {
+    this.lista = this.db.list("data/energia");
+  console.log("Dffdsdf " + this.lista);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EnergiaPage');
   }
 
+  
 }
